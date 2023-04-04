ce13e7357eea2fd450e0e34f0d6664117d3bc1b3
Andrea Manzoni <a.manzoni1@proton.me>
2023-04-04 17:35:21 +0200

    newsTerminal hook updated

--- client/src/hooks/useNewsTerminal.jsx
+++ client/src/hooks/useNewsTerminal.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from 'react';
 import useWebSocket from 'react-use-websocket';
 import useSymbols from './useSymbols';
-import useFutureSymbols from './useFutureSymbols';
 import config from '../config';
 
 const useNewsTerminal = () => {
   const [coins, setCoins] = useState('');
   const { data: symbols } = useSymbols();
-  const { data: futSymbols } = useFutureSymbols();
 
   const { sendMessage, lastMessage } = useWebSocket(config.wsUri, {
     shouldReconnect: (closeEvent) => true,
@@ -15,25 +13,51 @@ const useNewsTerminal = () => {
     reconnectInterval: 3000,
   });
 
-  const isCoinExistInSymbols = (coin) =>
-    symbols.find((symbol) => symbol.label === coin);
-
-  const coinWithOthersQuoteCurr = (coin) => {
-    const newCoin = coin.replace(/USDT/gi, 'BUSD');
-    symbols.find((symbol) => symbol.label === newCoin);
-  }
-
   useEffect(() => {
-    sendMessage('login 842752f3f9b8271110aa50829407762f536b8a34e43661db7f3e3ff4cb8ca772');
+    const res = sendMessage('login 842752f3f9b8271110aa50829407762f536b8a34e43661db7f3e3ff4cb8ca772');
   }, []);
 
   useEffect(() => {
-    if (lastMessage) {
+  if (lastMessage) {
+    const message = JSON.parse(lastMessage?.data);
+
+    if(message?.suggestions?.length > 0) {
+      //for (let i = 0; i < message?.suggestions?.length; i++) {
+        const coin = message.suggestions[0]['coin'];
+        const coinSym = coin + '/USDT';
+        setCoins(coinSym);
+      
+    }  else {
+      for (let x in symbols) {
+        let coin = symbols[x].label.replace(/\/USDT|\/BUSD/gi, '');
+        let coinEx = '[^A-Za-z0-9_]' + coin + '[^A-Za-z0-9_]';
+        let regex = new RegExp(coinEx, 'gi'); 
+        if (message?.title?.match(regex) || message?.body?.match(regex)) {
+          if(coin !== 'T' && coin !== 'FOR') {
+            const coinSy = coin + '/USDT';
+            setCoins(coinSy);
+          }
+        }
+      } 
+    }
+  }
+}, [lastMessage]);
+
+  return { coins };
+};
+
+export default useNewsTerminal;
+
+
+
+/*
+if (lastMessage) {
       const message = JSON.parse(lastMessage?.data);
       const action = message?.actions ? message?.actions[1]?.title : '';
 
       if (action && action?.length > 0) {
-        const newAction = action.replace(/\/.*/, '/USDT');
+        const newAction = action.replace(/\/.*/   
+       /*, '/USDT');      <---------------------------------------------------
         if (isCoinExistInSymbols(newAction)) {
           setCoins(newAction);
           return;
@@ -69,9 +93,7 @@ const useNewsTerminal = () => {
         }
      }  
     }
-  }, [lastMessage]);
+    */
 
-  return { coins };
-};
 
-export default useNewsTerminal;
+
